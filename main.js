b0e695b263d7710734d4d7ba68095388a9880d0a
Joel Bryla <joel@wiredlizard.com>
2017-08-30 19:31:09 +1000

    added JS

---
+++ main.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+
+$( "#left" ).click(function() {
+    $('.transform').toggleClass('transform-active');
+});
+
+$( "#right" ).click(function() {
+    $('.transform').toggleClass('transform-active');
+});
+
+
+});
