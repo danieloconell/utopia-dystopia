a5ba85156d31121e7cb445218ad4ad1684d2ff7d
Daniel O'Connell <dan.ben.oconnell@gmail.com>
2017-08-30 21:50:01 +1000

    Added utopia page

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ $( "#right" ).click(function() {
      $( ".transform2" ).animate({   
     width: "5000px"
   }, 2000);
-    setTimeout(function(){ document.location.href = "utopia.html"}, 2000)
+    setTimeout(function(){ document.location.href = "dystopia.html"}, 2000)
 
 });
 
