7bdd00295d82dfbe47dd3b69e2b892a0eabc2202
Joel <joel@localhost.localdomain>
2017-09-04 18:07:28 +1000

    added titles

--- main.js
+++ main.js
@@ -5,21 +5,14 @@ function change_page(page){
 }
 
 $( "#left" ).click(function() {
-    // $('.transform1').toggleClass('transform-active');
-    $( ".transform1" ).animate({
-    width: "5000px"
-  }, 2000);
-    setTimeout(function(){ document.location.href = "utopia.html"}, 2000)
+
+  setTimeout(function(){ document.location.href = "utopia.html"}, 2000)
     
 });
 
 $( "#right" ).click(function() {
-     $( ".transform2" ).animate({   
-    width: "5000px"
-  }, 2000);
-    setTimeout(function(){ document.location.href = "dystopia.html"}, 2000)
+
+  setTimeout(function(){ document.location.href = "dystopia.html"}, 2000)
 
 });
-
-
 });
